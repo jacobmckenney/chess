21d3171a001c803b0a84a61c9a27a037a178818b
Jacob McKenney <jacobmckenney@Jacobs-macbook.local>
2023-01-28 23:59:36 -0800

    all pawn validation except en passant

--- src/utils/move-validation.ts
+++ src/utils/move-validation.ts
@@ -1,5 +1,5 @@
 import { isEqual } from "lodash";
-import { Pawn, Bishop, Knight, Rook, Queen, King } from "../constants/board";
+import { Pawn, Bishop, Knight, Rook, Queen, King, Black, White } from "../constants/board";
 import type { Square, BoardState, Piece } from "../types/board";
 
 type Diffs = {
@@ -64,11 +64,19 @@ const isValidKnightMove = ({ diffX, diffY}: Diffs) => {
 }
 
 const isValidKingMove = (from: Square, to: Square, boardState: BoardState, { diffX, diffY }: Diffs) => {
-  return (diffX === 1 || diffY === 1) && !inCheck(from, to, boardState);
+  return (diffX === 1 || diffY === 1);
 }
 
-const isValidPawnMove = (from: Square, to: Square, diffs: Diffs) => {
-  return true;
+const isValidPawnMove = (from: Square, to: Square, { turn, board }: BoardState, diffs: Diffs) => {
+  const { diffY, diffX} = diffs;
+  const [{ absRow: fromRow}, {absCol: toCol, absRow: toRow}] = [from, to];
+  const rowDir = fromRow - toRow > 0 ? 1 : -1;
+  const isValidDirection = (rowDir > 0 && turn) || (rowDir < 0 && !turn);
+  const isValidNumSquares = (fromRow === (turn ? 6 : 1) && diffY <= 2) || diffY == 1;
+  // TODO: incorporate en passant
+  const isOneDiagonal = diffX === 1 && toRow === (fromRow + (turn ? - 1 : 1));
+  const canTake = isOneDiagonal && board[toRow][toCol]?.color === (turn ? Black : White);
+  return isValidDirection && isValidNumSquares && ((onCol(diffs) && !board[toRow][toCol]) || canTake);
 }
 // This will be a pretty complicated function, need to incorporate castling, en passant, knight moves, etc.
 // probably should just calculate all possible squares and just containment conditional so that the
@@ -80,13 +88,13 @@ export const isValidMove = (from: Square, to: Square, boardState: BoardState) =>
   const selectedPiece = board[fromRow][fromCol] as Piece;
   const { type, color } = selectedPiece;
   const diffs = getDiffs(from, to);
-  if (isEqual(from, to) || (color === board[toRow][toCol]?.color)) {
+  if (isEqual(from, to) || (color === board[toRow][toCol]?.color) || inCheck(from, to, boardState)) {
     return false;
   }
   switch (type) {
     case Pawn:
       // TODO: incorporate en passant, one-directional rule, capturing, and max number of squares
-      return isValidPawnMove(from, to, diffs);
+      return isValidPawnMove(from, to, boardState, diffs);
     case Bishop:
       return onDiagonal(diffs) && !isBlocked(from, to, boardState, diffs);
     case Knight:
